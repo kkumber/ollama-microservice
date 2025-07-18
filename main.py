2a58b96421eba1f6480cba89615d3a6e15cc54c7
kkumber <angelbertreyes28@gmail.com>
2025-07-18 18:14:14 +0800

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from routers import generate_questions
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(generate_questions.router)
---
+++ routers/generate_questions.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from typing import List, Dict, Any
+
+router = APIRouter()
